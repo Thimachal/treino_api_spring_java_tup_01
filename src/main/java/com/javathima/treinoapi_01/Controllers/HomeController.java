e872f445aa649adb9d5f7608dd9d8379571bc616
Thimachal <thimachal@gmail.com>
2023-10-25 19:47:21 -0300

    testando

--- src/main/java/com/javathima/treinoapi_01/Controllers/HomeController.java
+++ src/main/java/com/javathima/treinoapi_01/Controllers/HomeController.java
@@ -1,7 +1,16 @@
 package com.javathima.treinoapi_01.Controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller é utilizado para retornar views (normalmente HTML),
+//enquanto @RestController é utilizado para retornar dados serializados em formatos como JSON e XML
+@RestController
 public class HomeController {
+
+    @GetMapping("/")
+    public void test(){
+
+    }
+
 }
