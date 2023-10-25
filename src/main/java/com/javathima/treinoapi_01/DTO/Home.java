547fafd191acb8fff764635aba648d0e137dfd06
Thimachal <thimachal@gmail.com>
2023-10-25 20:14:45 -0300

    classeadd

--- src/main/java/com/javathima/treinoapi_01/DTO/Home.java
+++ src/main/java/com/javathima/treinoapi_01/DTO/Home.java
@@ -1,4 +1,8 @@
 package com.javathima.treinoapi_01.DTO;
-
+//a pasta pode ser DTO ou modelView do padrão MVC
 public class Home {
+    public getMenssenger(){
+        return "Este é um view model com padrão DTO";
+    }
+
 }
