dfb50bb933e888f8c63d9420b134202a1cf7ea91
Thimachal <thimachal@gmail.com>
2023-10-25 19:49:24 -0300

    testando2

--- src/main/java/com/javathima/treinoapi_01/Controllers/HomeController.java
+++ src/main/java/com/javathima/treinoapi_01/Controllers/HomeController.java
@@ -3,14 +3,13 @@ package com.javathima.treinoapi_01.Controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@Controller é utilizado para retornar views (normalmente HTML),
-//enquanto @RestController é utilizado para retornar dados serializados em formatos como JSON e XML
+//@Controller é utilizado para retornar views (normalmente HTML)
+//@RestController é mais simples, usado para retornar dados serializados em formatos como JSON e XML
 @RestController
 public class HomeController {
-
     @GetMapping("/")
-    public void test(){
-
+    public String index(){
+        return "Thima na área";
     }
 
 }
