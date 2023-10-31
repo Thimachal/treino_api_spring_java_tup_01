319ea64f99d9c7499d13807e107bdfd14620d865
Thimachal <thimachal@gmail.com>
2023-10-30 22:00:44 -0300

    controller

--- src/main/java/com/javathima/treinoapi_01/Controllers/ProfessionalsController.java
+++ src/main/java/com/javathima/treinoapi_01/Controllers/ProfessionalsController.java
@@ -1,20 +1,24 @@
 package com.javathima.treinoapi_01.Controllers;
 
+import com.javathima.treinoapi_01.DAOs.IProfessionalRepository;
 import com.javathima.treinoapi_01.Models.Professional;
-import com.javathima.treinoapi_01.Services.ProfessionalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 //@Controller é utilizado para retornar views (normalmente HTML)
 //@RestController é mais simples, usado para retornar dados serializados em formatos como JSON e XML
 @RestController
 public class ProfessionalsController {
 
-       @GetMapping("/professionals")
-    public List<Professional> professionals(){
-        return ProfessionalService.professionals();
+    @Autowired
+    private IProfessionalRepository repo;
+
+    @GetMapping("/professionals")
+    ResponseEntity<Iterable<Professional>> index(){
+        var professionals = repo.findAll();
+        return ResponseEntity.status(200).body(professionals);
     }
 
 }
