cd6770b7706f7d01c7d4684365364307725977a7
Thimachal <thimachal@gmail.com>
2023-10-30 22:00:28 -0300

    jpa

--- src/main/java/com/javathima/treinoapi_01/DAOs/IProfessionalRepository.java
+++ src/main/java/com/javathima/treinoapi_01/DAOs/IProfessionalRepository.java
@@ -3,5 +3,5 @@ package com.javathima.treinoapi_01.DAOs;
 import com.javathima.treinoapi_01.Models.Professional;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProfessionalRepository extends CrudRepository<Professional, Integer> {
+public interface IProfessionalRepository extends CrudRepository<Professional, Integer> {
 }
