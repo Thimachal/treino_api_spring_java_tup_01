ee6fc8f35dee350d4facd4987c96040de4d8e2d2
Thimachal <thimachal@gmail.com>
2023-10-30 21:45:24 -0300

    refactor

--- src/main/java/com/javathima/treinoapi_01/Models/Professional.java
+++ src/main/java/com/javathima/treinoapi_01/Models/Professional.java
@@ -1,24 +1,31 @@
 package com.javathima.treinoapi_01.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Professional {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false, length = 100)
     private String name;
+
+    @Column(nullable = false)
+    public LocalDateTime getDateCriation() {
+        return dateCriation;
+    }
+
+    public void setDateCriation(LocalDateTime dateCriation) {
+        this.dateCriation = dateCriation;
+    }
+
+    private LocalDateTime dateCriation;
 
     public Professional(){
 
-    }
-    public Professional(Integer id, String name) {
-        this.id = id;
-        this.name = name;
     }
 
     public Integer getId() {
