8826a28cd3631b3fa4227d7e0af0f4171748390b
Thimachal <thimachal@gmail.com>
2023-10-30 21:44:59 -0300

    refactor

--- src/main/java/com/javathima/treinoapi_01/Controllers/HomeController.java
+++ src/main/java/com/javathima/treinoapi_01/Controllers/HomeController.java
@@ -1,12 +1,8 @@
 package com.javathima.treinoapi_01.Controllers;
 
-import com.javathima.treinoapi_01.DTO.Home;
-import com.javathima.treinoapi_01.Models.Professional;
-import com.javathima.treinoapi_01.Services.ProfessionalService;
+import com.javathima.treinoapi_01.DTOs.Home;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 //@Controller é utilizado para retornar views (normalmente HTML)
 //@RestController é mais simples, usado para retornar dados serializados em formatos como JSON e XML
